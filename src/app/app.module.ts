33404541e18cda36b941507f0f677a47815f2a80
Mohamad <m_haidouss@hotmail.com>
2023-06-28 19:52:58 +0200

    new ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -7,7 +6,12 @@ import { SearchPageComponent } from './components/search/search-page/search-page
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { MatCardModule } from '@angular/material/card';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
+  schemas: [NO_ERRORS_SCHEMA],
   declarations: [
     AppComponent,
     SearchPageComponent
@@ -20,7 +24,9 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
     HttpClientModule,
     BrowserAnimationsModule,
     SlickCarouselModule,
-    SlickCarouselModule
+    MatCardModule,
+    MatAutocompleteModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/search/search-page/search-page.component.ts
+++ src/app/components/search/search-page/search-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Product } from 'src/app/Interfaces/ProductInterface';
 import { ResponseMessage } from 'src/app/Interfaces/responseMessage';
@@ -6,20 +6,91 @@ import { SearchedByCategory } from 'src/app/Interfaces/searchedByCategoryInterfa
 import { SearchedProduct } from 'src/app/Interfaces/searchedProduct';
 import { SearchedProductByCount } from 'src/app/Interfaces/SearchedProductInterface';
 import { SearchService } from 'src/app/services/search.service';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.css']
 })
-export class SearchPageComponent  {
-  slides = [
-    { title: 'Slide 1', description: 'This is the first slide' },
-    { title: 'Slide 2', description: 'This is the second slide' },
-    { title: 'Slide 3', description: 'This is the third slide' },
-    // Add more slides as needed
+export class SearchPageComponent {
+  fruits = [
+    'Apple',
+    'Banana',
+    'Cherry',
+    'Date',
+    'Elderberry',
+    'Fig',
+    'Grape',
+    'Lemon',
+    'Mango',
+    'Orange',
+    'Papaya',
+    'Strawberry',
   ];
+  
+  displayedCards: any[] = [];
+  translateValue = 0;
+  cardWidth = 0; // Add the cardWidth property
+
+  @ViewChild('cardContainer') cardContainer!: ElementRef;
+
+
+  cards = [
+    {
+      title: 'Card 1',
+      description: 'This is the description for card 1.'
+    },
+    {
+      title: 'Card 2',
+      description: 'This is the description for card 2.'
+    },
+    {
+      title: 'Card 2',
+      description: 'This is the description for card 2.'
+    },
+    {
+      title: 'Card 2',
+      description: 'This is the description for card 2.'
+    },
+    {
+      title: 'Card 2',
+      description: 'This is the description for card 2.'
+    },
+    {
+      title: 'Card 2',
+      description: 'This is the description for card 2.'
+    },
+    {
+      title: 'Card 2',
+      description: 'This is the description for card 2.'
+    },
+    {
+      title: 'Card 2',
+      description: 'This is the description for card 2.'
+    },
+    {
+      title: 'Card 2',
+      description: 'This is the description for card 2.'
+    },
+    {
+      title: 'Card 2',
+      description: 'This is the description for card 2.'
+    },
+    {
+      title: 'Card 2',
+      description: 'This is the description for card 2.'
+    },
+    {
+      title: 'Card 2',
+      description: 'This is the description for card 2.'
+    },
+    // Add more card objects as needed
+  ];
+
+
+  
+  
+
   searchedForm!: FormGroup;
 
   products!: Product[];
@@ -33,11 +104,11 @@ export class SearchPageComponent  {
       });
   
     }
-
+ 
   ngOnInit(): void {
-    //this.fetchDataFromApi() 
+    this.fetchDataFromApi() 
     //this.fetchByMostSearched()
-
+  
     
   }
 
@@ -121,9 +192,42 @@ toggleData(): void {
     this.getByCategory();
   }
 }
+// Slider navigation
 
+ngAfterViewInit() {
+  this.updateDisplayedCards();
+}
 
+slide(direction: 'next' | 'prev') {
+  const cardContainerElement = this.cardContainer.nativeElement;
+  const cardElements = cardContainerElement.querySelectorAll('.card');
+  this.cardWidth = cardElements.length > 0 ? cardElements[0].clientWidth : 0;
 
+  const containerWidth = cardContainerElement.clientWidth;
+  const visibleCards = 3;
+  const maxTranslateValue = (this.cardWidth * this.cards.length) - (this.cardWidth * visibleCards);
+
+  if (direction === 'next') {
+    this.translateValue -= this.cardWidth * visibleCards;
+    if (this.translateValue < -maxTranslateValue) {
+      this.translateValue = -maxTranslateValue;
+    }
+  } else if (direction === 'prev') {
+    this.translateValue += this.cardWidth * visibleCards;
+    if (this.translateValue > 0) {
+      this.translateValue = 0;
+    }
+  }
+
+  cardContainerElement.style.transform = `translateX(${this.translateValue}px`; // Update the transform value
+
+  this.updateDisplayedCards();
+}
+
+updateDisplayedCards() {
+  const startIndex = Math.abs(Math.floor(this.translateValue / this.cardWidth));
+  this.displayedCards = this.cards.slice(startIndex, startIndex + 3);
+}
 }
 
 
